d3c39224656e31b22bf1b8187c6976a11312a8bb
ellifteria <ellifteria@users.noreply.github.com>
2024-10-24 06:28:43 +0000

    deploy: 6bb616d0dfdf387ea5638af6347f504a76420b6f

--- js/colorChanger.js
+++ js/colorChanger.js
@@ -1,20 +1,12 @@
 var main = function(){
     let style = document.getElementById("additionalStyle");
     let colors = [
-        "#4169E1",
-        "#40B5AD",
-        "#088F8F",
-        "#009E60",
-        "#EC5800",
-        "#DA70D6",
-        "#C21E56",
-        "#9F2B68",
-        "#8D029B",
-        "#D473D4",
-        "#9A4EAE",
-        "#EF9B0F",
-        "#DAA520",
-        "#D4AF37"
+        "#D52D00",
+        "#EF7627",
+        "#FF9A56",
+        "#D162A4",
+        "#B55690",
+        "#A30262"
     ];
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
     style.innerHTML = `
